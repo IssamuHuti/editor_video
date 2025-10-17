feed574aaa08339e2bf9e8f601ad4a9d65e22f82
IssamuHuti <lucas.issamum@gmail.com>
2025-10-16 22:24:10 -0300

    16/10/25

--- modelo_do_zero.py
+++ modelo_do_zero.py
@@ -1,11 +1,11 @@
 import sys
 import os
 from PySide6.QtWidgets import (
-    QApplication, QMainWindow, QWidget,
+    QApplication, QMainWindow, QWidget, QFrame, QListWidget,
     QVBoxLayout, QHBoxLayout, QToolBar, QSlider,
     QStackedWidget, QLabel, QPushButton, QFileDialog
 )
-from PySide6.QtCore import Qt
+from PySide6.QtCore import Qt, Signal
 from PySide6.QtGui import QAction
 from PySide6.QtMultimedia import QMediaPlayer, QAudioOutput
 from PySide6.QtMultimediaWidgets import QVideoWidget
@@ -31,22 +31,31 @@ class TelaPrincipal(QMainWindow):
         acaoPaginaAudio      = QAction('Audio', self)
         acaoPaginaPosicao    = QAction('Posicao', self)
         acaoPaginaIluminacao = QAction('Iluminacao', self)
+        acaoPaginaAnimacao   = QAction('Animacao', self)
+        acaoPaginaConcatenar = QAction('Concatenar', self)
+        acaoPaginaAplicacoes = QAction('Aplicacoes', self)
 
         acaoPaginaRecortar.triggered.connect(lambda: self.stack.setCurrentIndex(0))
         acaoPaginaAudio.triggered.connect(lambda: self.stack.setCurrentIndex(1))
         acaoPaginaPosicao.triggered.connect(lambda: self.stack.setCurrentIndex(2))
         acaoPaginaIluminacao.triggered.connect(lambda: self.stack.setCurrentIndex(3))
+        acaoPaginaAnimacao.triggered.connect(lambda: self.stack.setCurrentIndex(4))
+        acaoPaginaConcatenar.triggered.connect(lambda: self.stack.setCurrentIndex(5))
+        acaoPaginaAplicacoes.triggered.connect(lambda: self.stack.setCurrentIndex(6))
 
         self.menuVertical.addAction(acaoPaginaRecortar)
         self.menuVertical.addAction(acaoPaginaAudio)
         self.menuVertical.addAction(acaoPaginaPosicao)
         self.menuVertical.addAction(acaoPaginaIluminacao)
+        self.menuVertical.addAction(acaoPaginaAnimacao)
+        self.menuVertical.addAction(acaoPaginaConcatenar)
+        self.menuVertical.addAction(acaoPaginaAplicacoes)
 
         layoutPrincipal.addWidget(self.menuVertical, 1.5)
 
         layoutCentral = QVBoxLayout()
 
-        self.videoWidget = QVideoWidget(self)
+        self.videoWidget = VideoWidgetInterativo(self)
         self.player      = QMediaPlayer(self)
         self.audioOutput = QAudioOutput(self)
         self.player.setAudioOutput(self.audioOutput)
@@ -85,21 +94,34 @@ class TelaPrincipal(QMainWindow):
         self.stack.addWidget(Audio())
         self.stack.addWidget(Posicao())
         self.stack.addWidget(Iluminacao())
+        self.stack.addWidget(Animacao())
+        self.stack.addWidget(Concatenar())
+        self.stack.addWidget(Aplicacoes())
 
         layoutCentral.addWidget(self.stack, 2)
         layoutPrincipal.addLayout(layoutCentral, 7)
 
         layoutListas = QVBoxLayout()
         botaoCarregarVideo = QPushButton('Carregar Video')
+        self.caixaBotaoRecortes = CaixaLista('Recortados', [0, 1, 2, 3])
+        self.caixaBotaoEditados = CaixaLista('Editados', [0, 1, 2, 3])
+        self.caixaVideoMemes = CaixaLista('Memes', [0, 1, 2, 3])
+        self.caixaBotaoSalvos = CaixaLista('Salvos', [0, 1, 2, 3])
 
         layoutListas.addWidget(botaoCarregarVideo)
-        layoutListas.addWidget(QLabel('Listas'))
+        layoutListas.addWidget(self.caixaBotaoRecortes)
+        layoutListas.addWidget(self.caixaBotaoEditados)
+        layoutListas.addWidget(self.caixaVideoMemes)
+        layoutListas.addWidget(self.caixaBotaoSalvos)
+        layoutListas.addStretch()
 
         layoutPrincipal.addLayout(layoutListas, 1.5)
 
-        botaoCarregarVideo.clicked.connect(self.abrirVideo)
-        self.volumeSlider.valueChanged.connect(self.ajusteVolume)
-        self.player.positionChanged.connect(self.atualizarSlider)
+        botaoCarregarVideo.clicked.connect(self.abrirVideo)       # carrega um video do computador
+        self.volumeSlider.valueChanged.connect(self.ajusteVolume) # regula o volume do video
+        self.player.positionChanged.connect(self.atualizarSlider) # atualiza o slider conforme video roda
+        self.slider.sliderMoved.connect(self.player.setPosition)  # mover o slider permite alterar o momento do video
+        self.videoWidget.select.connect(self.alternarPlayPause)   # pausa e continua o video
 
     def abrirVideo(self):
         arquivo, _ = QFileDialog.getOpenFileName( # serve para selecionar arquivos de vídeo
@@ -121,13 +143,19 @@ class TelaPrincipal(QMainWindow):
         tempoTotal = QTime(0, 0, 0).addMSecs(duracao)
         self.temporizador.setText(f"{tempoAtual.toString('hh:mm:ss')} / {tempoTotal.toString('hh:mm:ss')}")
 
+    def alternarPlayPause(self):
+        if self.player.playbackState() == QMediaPlayer.PlayingState:
+            self.player.pause()
+        else:
+            self.player.play()
+
 
 class Recortar(QWidget):
     def __init__(self):
         super().__init__()
         layoutRecortar = QVBoxLayout(self)
         layoutRecortar.addWidget(QLabel('Recorte'))
-        layoutRecortar.addWidget(QPushButton('Salvar'))
+        layoutRecortar.addWidget(QPushButton('Aplicar'))
 
 
 class Audio(QWidget):
@@ -135,7 +163,7 @@ class Audio(QWidget):
         super().__init__()
         layoutAudio = QVBoxLayout(self)
         layoutAudio.addWidget(QLabel('Audio'))
-        layoutAudio.addWidget(QPushButton('Salvar'))
+        layoutAudio.addWidget(QPushButton('Aplicar'))
 
 
 class Posicao(QWidget):
@@ -143,7 +171,7 @@ class Posicao(QWidget):
         super().__init__()
         layoutPosicao = QVBoxLayout(self)
         layoutPosicao.addWidget(QLabel('Posicao'))
-        layoutPosicao.addWidget(QPushButton('Salvar'))
+        layoutPosicao.addWidget(QPushButton('Aplicar'))
 
 
 class Iluminacao(QWidget):
@@ -151,7 +179,31 @@ class Iluminacao(QWidget):
         super().__init__()
         layoutIluminacao = QVBoxLayout(self)
         layoutIluminacao.addWidget(QLabel('Iluminacao'))
-        layoutIluminacao.addWidget(QPushButton('Salvar'))
+        layoutIluminacao.addWidget(QPushButton('Aplicar'))
+
+
+class Animacao(QWidget):
+    def __init__(self):
+        super().__init__()
+        layoutAnimacao = QVBoxLayout(self)
+        layoutAnimacao.addWidget(QLabel('Animacao'))
+        layoutAnimacao.addWidget(QPushButton('Aplicar'))
+
+
+class Concatenar(QWidget):
+    def __init__(self):
+        super().__init__()
+        layoutConcatenar = QVBoxLayout(self)
+        layoutConcatenar.addWidget(QLabel('Concatenar'))
+        layoutConcatenar.addWidget(QPushButton('Aplicar'))
+
+
+class Aplicacoes(QWidget):
+    def __init__(self):
+        super().__init__()
+        layoutAplicacoes = QVBoxLayout(self)
+        layoutAplicacoes.addWidget(QLabel('Aplicacoes'))
+        layoutAplicacoes.addWidget(QPushButton('Aplicar'))
 
 
 class ConfigSlider(QSlider):
@@ -187,6 +239,43 @@ class ConfigSlider(QSlider):
 
     def leaveEvent(self, event):
         self.caixaFlutuante.setVisible(False)
+
+
+class CaixaLista(QFrame):
+    def __init__(self, titulo, opcoes, funcaoExpandir=None):
+        super().__init__()
+        self.expandirLista = funcaoExpandir
+
+        self.botaoLista = QPushButton(titulo)
+        self.lista = QListWidget()
+        self.lista.addItems(opcoes)
+        self.lista.setVisible(False)
+
+        layoutCaixaLista = QVBoxLayout(self)
+        layoutCaixaLista.setContentsMargins(0, 0, 0, 0)
+        layoutCaixaLista.addWidget(self.botaoLista)
+        layoutCaixaLista.addWidget(self.lista)
+
+        self.botaoLista.clicked.connect(self.esconderLista)
+
+    def esconderLista(self):
+        self.lista.setVisible(not self.lista.isVisible())
+        if self.expandirLista:
+            self.expandirLista()
+    
+    def listaExpandida(self):
+        return self.lista.isVisible()
+    
+    def alturaExpansao(self):
+        return self.botaoLista.height() + (self.lista.sizeHintForRow(0) * self.lista.count() if self.listaExpandida() else 0)
+
+
+class VideoWidgetInterativo(QVideoWidget):
+    select = Signal()  # Sinal personalizado
+
+    def mousePressEvent(self, event):
+        self.select.emit()  # Emite o sinal quando o vídeo for clicado
+        super().mousePressEvent(event)
 
 
 if __name__ == "__main__":
